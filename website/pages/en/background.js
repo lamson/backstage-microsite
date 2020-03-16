5e997f6e03e8da0558fcfb7afdf2c9ea6d43723a
Patrik Oldsberg <poldsberg@gmail.com>
2020-03-16 14:37:49 +0100

    story: backgrounds n text n stuff (#85)

--- website/pages/en/background.js
+++ website/pages/en/background.js
@@ -23,16 +23,10 @@ const Background = (props) => {
         </Block.Container>
       </Block>
 
-      <Block small className="stripe bg-black">
-        <Block.Quote>
-          Openness, community, and extendability make up the foundation of Backstage.
-        </Block.Quote>
-      </Block>
-
       <Block className="stripe bg-black">
         <Block.Container>
           <Block.TitleBox>Productivity and happiness, together again</Block.TitleBox>
-          <Block.TextBox>
+          <Block.TextBox wide>
             <Block.Paragraph>Since adopting Backstage internally at Spotify, we’ve seen a 55% decrease in onboarding time for our engineers (as measured by time until 10th pull request).</Block.Paragraph>
             <Block.Paragraph>For a company growing as fast as ours, this is a game-changing improvement to both productivity and developer happiness — which we believe go hand in hand.</Block.Paragraph>
             <Block.Paragraph>Instead of keeping all those gains for ourselves, we decided to share that developer joy by making the Backstage platform open source. After all, openness, community, and extendability are all at the heart of Backstage.</Block.Paragraph>
@@ -40,13 +34,29 @@ const Background = (props) => {
         </Block.Container>
       </Block>
 
+      <Block small className="stripe bg-black">
+        <Block.Quote>
+          Openness, community, and extendability make up the foundation of Backstage.
+        </Block.Quote>
+      </Block>
+
       <Block className="stripe bg-grey-black">
         <Block.Container>
           <Block.TitleBox>Why did we build it?</Block.TitleBox>
-          <Block.TextBox>
+          <Block.TextBox wide>
             <Block.Paragraph>Short answer: we had to in order to reduce friction for our engineers.</Block.Paragraph>
             <Block.Paragraph>From the start, engineering teams at Spotify have been organized almost like independent states — each with a distinct mission and the freedom to decide how to best fulfil that mission. From many self-sufficient teams come many innovations.</Block.Paragraph>
             <Block.Paragraph>But as the number of teams grew — and the number of tools they built grew with them — our ecosystem became too complex. Fragmentation was increasing the cognitive load on engineers. We started seeing a lot more friction and engineers having to sort problems outside of their main scope.</Block.Paragraph>
+          </Block.TextBox>
+        </Block.Container>
+      </Block>
+
+      <Block className="stripe bg-black">
+        <Block.Container>
+          <Block.TitleBox>A platform for platforms</Block.TitleBox>
+          <Block.TextBox wide>
+            <Block.Paragraph>Our idea was to unify all our tooling, services, and documentation into a single location, with a single interface, behind a single layer of abstraction. So it’s easy for engineers to share solutions and even easier for anyone to find them. Not another tool, but a better toolbox. A one-stop shop for all things infrastructure. A platform of platforms.</Block.Paragraph>
+            <Block.Paragraph>Leveraging the simplicity of plugins and a thoughtful UI, now every question has the same answer. How do I find out how many pods service X is running in production? Go to Backstage. How do I integrate this data set into my app? Backstage. Is this ML service acting weird for anyone else? Figure it out on Backstage.</Block.Paragraph>
           </Block.TextBox>
         </Block.Container>
       </Block>
@@ -57,7 +67,7 @@ const Background = (props) => {
         </Block.Quote>
       </Block>
 
-      <Block className="stripe bg-dots bg-dots-opacity">
+      <Block small className="stripe bg-dots bg-dots-opacity">
         <Block.Container className="opacity-override">
           <Block.TitleBox>Not just adopted, embraced</Block.TitleBox>
           <Block.TextBox>
@@ -67,7 +77,7 @@ const Background = (props) => {
         </Block.Container>
       </Block>
 
-      <Block className="stripe bg-dots">
+      <Block small className="stripe bg-dots">
         <img src={`${baseUrl}img/embraced.svg`}/>
       </Block>
 
