f8ea0737d43c6376624bf8793cfa956c2797e716
Stefan Ålund <alund@spotify.com>
2020-03-16 11:59:05 +0100

    Fix copy and blocks on background (#60)

--- website/pages/en/background.js
+++ website/pages/en/background.js
@@ -40,7 +40,9 @@ const Background = (props) => {
             <Block.Title>Productivity and happiness, together again</Block.Title>
           </Block.TextBox>
           <Block.TextBox>
-            <Block.Paragraph>Since adopting Backstage internally at Spotify, we’ve seen a 55% decrease in onboarding time for our engineers (as measured by time until 10th pull request). For a company growing as fast as ours, this is a game-changing improvement to both productivity and developer happiness — which we believe go hand in hand. Instead of keeping all those gains for ourselves, we decided to share that developer joy by making the Backstage platform open source. After all, openness, community, and extendability are all at the heart of Backstage.</Block.Paragraph>
+            <Block.Paragraph>Since adopting Backstage internally at Spotify, we’ve seen a 55% decrease in onboarding time for our engineers (as measured by time until 10th pull request).</Block.Paragraph>
+            <Block.Paragraph>For a company growing as fast as ours, this is a game-changing improvement to both productivity and developer happiness — which we believe go hand in hand.</Block.Paragraph>
+            <Block.Paragraph>Instead of keeping all those gains for ourselves, we decided to share that developer joy by making the Backstage platform open source. After all, openness, community, and extendability are all at the heart of Backstage.</Block.Paragraph>
           </Block.TextBox>
         </Block.Container>
       </Block>
@@ -51,7 +53,9 @@ const Background = (props) => {
             <Block.Title>Why did we build it?</Block.Title>
           </Block.TextBox>
           <Block.TextBox>
-            <Block.Paragraph>Short answer: We had to for the sanity of our engineers. From the start, engineering teams at Spotify have been organized almost like tiny independent states — each with a distinct mission and the freedom to decide how to best fulfil that mission. From many self-sufficient teams come many innovations. But as the number of teams grew — and the number of tools they built grew with them — our ecosystem became too complex. Fragmentation was increasing the cognitive load on engineers. We started seeing development slow down and engineers getting more frustrated. A very bad, no-good cycle.</Block.Paragraph>
+            <Block.Paragraph>Short answer: we had to in order to reduce friction for our engineers.</Block.Paragraph>
+            <Block.Paragraph>From the start, engineering teams at Spotify have been organized almost like independent states — each with a distinct mission and the freedom to decide how to best fulfil that mission. From many self-sufficient teams come many innovations.</Block.Paragraph>
+            <Block.Paragraph>But as the number of teams grew — and the number of tools they built grew with them — our ecosystem became too complex. Fragmentation was increasing the cognitive load on engineers. We started seeing a lot more friction and engineers having to sort problems outside of their main scope.</Block.Paragraph>
           </Block.TextBox>
         </Block.Container>
       </Block>
@@ -74,7 +78,8 @@ const Background = (props) => {
             <Block.Title>Not just adopted, embraced</Block.Title>
           </Block.TextBox>
           <Block.TextBox>
-            <Block.Paragraph>In 2019, more than 200 engineers inside Spotify contributed to Backstage. We now have 110+ plugins developed by 50+ teams. And 80% of contributions came from people outside the Backstage core team. Any Spotifier, not just engineers, but also compliance and security team members, can easily discover all the software in our ecosystem, see who owns it, and access technical documentation in a centralised location.</Block.Paragraph>
+            <Block.Paragraph>In 2019, more than 200 engineers inside Spotify contributed to Backstage. We now have 110+ plugins developed by 50+ teams. And 80% of contributions came from people outside the Backstage core team.</Block.Paragraph>
+            <Block.Paragraph>Any Spotifier, not just engineers, but also compliance and security team members, can easily discover all the software in our ecosystem, see who owns it, and access technical documentation in a centralised location.</Block.Paragraph>
           </Block.TextBox>
         </Block.Container>
       </Block>
@@ -89,7 +94,8 @@ const Background = (props) => {
             <Block.Title>T-shaped engineers, lighter cognitive loads </Block.Title>
           </Block.TextBox>
           <Block.TextBox>
-            <Block.Paragraph>By providing a consistent experience, even across domains, Backstage helps engineers be T-shaped. They don’t need to learn an entirely new set of tools, each with a different UX. Because the tools for troubleshooting a failing data pipeline are similar enough to the ones for deploying a website, a T-shaped web engineer is able to help out with basic data engineering tasks. A familiar, well-designed developer experience reduces the cognitive load on engineers. They spend less time searching and more time building. More energy is spent at the top of the stack instead of the bottom. Features bloom. And developer happiness goes up along with productivity.</Block.Paragraph>
+            <Block.Paragraph>By providing a consistent experience, even across domains, Backstage helps engineers be T-shaped. They don’t need to learn an entirely new set of tools, each with a different UX. Because the tools for troubleshooting a failing data pipeline are similar enough to the ones for deploying a website, a T-shaped web engineer is able to help out with basic data engineering tasks.</Block.Paragraph>
+            <Block.Paragraph>A familiar, well-designed developer experience reduces the cognitive load on engineers. They spend less time searching and more time building. More energy is spent at the top of the stack instead of the bottom. Features bloom. And developer happiness goes up along with productivity.</Block.Paragraph>
           </Block.TextBox>
         </Block.Container>
       </Block>
