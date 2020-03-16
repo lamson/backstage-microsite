18709a1e34c2ee4ecb6dba81a13d97fa297c44fd
Kai Mallea <kai@spotify.com>
2020-03-16 17:57:03 -0400

    Specify an image for Open Graph and Twitter cards (#94)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -90,8 +90,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: "img/undraw_online.svg",
-  twitterImage: "img/undraw_tweetstorm.svg",
+  ogImage: "img/backstage-repo-card-640x320.png",
+  twitterImage: "img/backstage-repo-card-640x320.png",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
