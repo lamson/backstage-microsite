97c01894d72ef6cbd081be6f311b036928182e8d
Stefan Ålund <alund@spotify.com>
2020-04-21 14:55:15 +0200

    Add 3rd demo video (#148)
    * Add 3rd demo video
    
    * Review comment

--- website/pages/en/demos.js
+++ website/pages/en/demos.js
@@ -18,13 +18,14 @@ const Background = (props) => {
       <Block small className="stripe-bottom bg-black-grey">
         <Block.Container style={{justifyContent: "flex-start"}}>
           <Block.TextBox>
-            <Block.Title>Create a microservice</Block.Title>
-            <Block.Paragraph>You’re a Spotify engineer about to build a new microservice (or any component) using Spring Boot. Where do you start? Search for a quick start guide online? Create an empty repo on GitHub? Copy and paste an old project? Nope. Just go to Backstage, and you’ll be up and running in two minutes — with a “Hello World” app, CI, and documentation all automatically set up and configured in a standardized way.</Block.Paragraph>
-
+            <Block.Title>Search all your services</Block.Title>
+            <Block.Paragraph>
+              All of Spotify’s services are automatically indexed in Backstage. So our engineers can stop playing detective — no more spamming Slack channels asking if anyone knows who owns a particular service and where you can find its API, only to discover that the owner went on sabbatical three months ago and you have to hunt them down on a mountain in Tibet where they’re on a 12-day silent meditation retreat. At Spotify, anyone can always find anyone else’s service, inspect its APIs, and contact its current owner — all with one search.
+            </Block.Paragraph>
             <Block.LinkButton href={"https://www.youtube.com/watch?v=U1iwe3L5pzc"}>Request a live demo</Block.LinkButton>
           </Block.TextBox>
           <Block.TextBox>
-            <iframe width="800" height="500" src="https://www.youtube.com/embed/U1iwe3L5pzc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="800" height="500" src="https://www.youtube.com/embed/vcDL9tOv7Eo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </Block.TextBox>
         </Block.Container>
       </Block>
@@ -40,6 +41,20 @@ const Background = (props) => {
           <iframe width="800" height="500" src="https://www.youtube.com/embed/rH46MLNZIPM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Block.TextBox>
       </Block.Container>
+      </Block>
+
+      <Block small className="stripe-bottom bg-black-grey">
+        <Block.Container style={{justifyContent: "flex-start"}}>
+          <Block.TextBox>
+            <Block.Title>Create a microservice</Block.Title>
+            <Block.Paragraph>You’re a Spotify engineer about to build a new microservice (or any component) using Spring Boot. Where do you start? Search for a quick start guide online? Create an empty repo on GitHub? Copy and paste an old project? Nope. Just go to Backstage, and you’ll be up and running in two minutes — with a “Hello World” app, CI, and documentation all automatically set up and configured in a standardized way.</Block.Paragraph>
+
+            <Block.LinkButton href={"https://www.youtube.com/watch?v=U1iwe3L5pzc"}>Request a live demo</Block.LinkButton>
+          </Block.TextBox>
+          <Block.TextBox>
+            <iframe width="800" height="500" src="https://www.youtube.com/embed/U1iwe3L5pzc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </Block.TextBox>
+        </Block.Container>
       </Block>
     </div>
   );
